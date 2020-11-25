ab7af8ce5ef6c3f1f0b0f0dbe9324d1d3c8f84ae
Andrew Malota <2bitoperations@gmail.com>
2020-11-25 16:53:04 -0600

    tweak

--- raspisump/reading.py
+++ raspisump/reading.py
@@ -94,15 +94,17 @@ def initialstate_stream_reading(reading):
 
 
 def json_file_reading(reading):
-    if "jsonfile.enabled" not in configs or configs["jsonfile.enabled"] != 1:
-        return
-
-    reading_for_json = {"level": reading,
-                        "timestamp": time.time() * 1000}
     try:
+        if "jsonfile.enabled" not in configs or configs["jsonfile.enabled"] != 1:
+            return
+
+        reading_for_json = {"level": reading,
+                            "timestamp": time.time() * 1000}
+        
         with open(configs["jsonfile.path"], mode='w') as jsonfile:
             json.dump(reading_for_json, jsonfile)
         os.chmod(configs["jsonfile.path"], stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP | stat.S_IROTH)
+        log.log_restarts("wrote json file to {filename}".format(filename=configs["jsonfile.path"]))
     except Exception as ex:
         log.log_errors("problem streaming to json file!")
 
